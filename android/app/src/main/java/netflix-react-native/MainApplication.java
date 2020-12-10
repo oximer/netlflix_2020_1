4f0c923a1eba950a55f809371eddef325a2749bc
F5913879 Jose Urbano Duarte Junior <urbano@bb.com.br>
2020-12-10 14:27:44 -0300

    configuracoes no android

--- android/app/src/main/java/netflix-react-native/MainApplication.java
+++ android/app/src/main/java/netflix-react-native/MainApplication.java
@@ -20,7 +20,9 @@ import org.unimodules.core.interfaces.SingletonModule;
 import expo.modules.constants.ConstantsPackage;
 import expo.modules.permissions.PermissionsPackage;
 import expo.modules.filesystem.FileSystemPackage;
-import expo.modules.updates.UpdatesController;
+
+import com.microsoft.codepush.react.CodePush;
+
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -52,23 +54,14 @@ public class MainApplication extends Application implements ReactApplication {
     }
 
     @Override
-    protected @Nullable String getJSBundleFile() {
-      if (BuildConfig.DEBUG) {
-        return super.getJSBundleFile();
-      } else {
-        return UpdatesController.getInstance().getLaunchAssetFile();
-      }
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
     }
 
     @Override
     protected @Nullable String getBundleAssetName() {
-      if (BuildConfig.DEBUG) {
-        return super.getBundleAssetName();
-      } else {
-        return UpdatesController.getInstance().getBundleAssetName();
-      }
-    }
-  };
+      return super.getBundleAssetName();
+    };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -79,11 +72,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
-
-    if (!BuildConfig.DEBUG) {
-      UpdatesController.initialize(this);
-    }
   }
 
   /**
